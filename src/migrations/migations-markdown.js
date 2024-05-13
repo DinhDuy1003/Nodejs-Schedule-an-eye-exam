3e756aa5a059cf7a0f639a12ac5fd06006361da2
Dinh Duy <dangdinhduy3939@gmail.com>
2024-05-13 20:16:21 +0700

    markdown

---
+++ src/migrations/migations-markdown.js
@@ -0,0 +1,51 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('markdowns', {
+    
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      doctorId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      specialtyId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      clinicId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      description: {
+        allowNull: true,
+        type: Sequelize.TEXT('long'),
+      },
+      contentMardown: {
+        allowNull: false,
+        type: Sequelize.TEXT('long'),
+      },
+      contentHTML: {
+        allowNull: false,
+        type: Sequelize.TEXT('long'),
+      },
+      
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('markdowns');
+  }
+};
