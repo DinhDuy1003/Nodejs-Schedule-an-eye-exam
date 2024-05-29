09cb0bf19e90da6d40d12bfba7e762a71dabf06a
Dinh Duy <dangdinhduy3939@gmail.com>
2024-05-29 08:53:22 +0700

    doctor infor3

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -155,8 +155,18 @@ let getDetailDoctorById = (inputId) =>{
                         exclude: ['password']
                     },
                     include: [
-                    {model:db.Markdown , attributes:['description','contentHTML','contentMardown']},
+                    {   model:db.Markdown , 
+                        attributes:['description','contentHTML','contentMardown']
+                    },
                     { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
+
+                    {model :db.Doctor_Infor, attributes:{exclude:['id','doctorId']},
+                    include:[
+                        { model: db.Allcode, as: 'priceTypeData', attributes: ['valueEn', 'valueVi'] },
+                        { model: db.Allcode, as: 'provinceTypeData', attributes: ['valueEn', 'valueVi'] },
+                        { model: db.Allcode, as: 'paymentTypeData', attributes: ['valueEn', 'valueVi'] },
+                        ]               
+                    },
                     ],
                     raw: false,
                     nest: true
